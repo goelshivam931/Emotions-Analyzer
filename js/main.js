62b83bb8c8d5987947401d8ea200ce302c4d3a71
Shivam Goel <56171542+goelshivam931@users.noreply.github.com>
2021-10-20 23:51:34 +0530

    Emotions setting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const video = document.getElementById("video");
-let predictedAges = [];
+
 
 // This basically loades all the models before starting the video.
 Promise.all([
@@ -20,11 +20,31 @@ function startVideo() {
   );
 }
 
+video.addEventListener("playing", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
 
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
 
+  setInterval(async () => {
 
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks() // To detect face landmarks
+      .withFaceExpressions() // To detect face expressions
+      .withAgeAndGender();   // To detect age
 
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
+    // To clear the previously made rectangle
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+  }, 1000);
+});
 
 
